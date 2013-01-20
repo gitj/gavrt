cc56155f10d0516aa96db5f7ab53a3d629d68a87
Glenn <glenn.caltech@gmail.com>
2013-01-20 05:42:58 -0500

    adding gavrt_constants.py

---
+++ grasp/gavrt_constants.py
@@ -0,0 +1,11 @@
+""" Constants of the GAVRT system """
+
+#Map of ibob to fiber
+ibob_fiber_map = {0:6,
+              1:7,
+              2:0,
+              3:1,
+              4:5,
+              5:4,
+              6:2,
+              7:3}
